8a0b97246e14721d5124bb724ec6768750e351c0
Redakyr <stakarovburgess@proton.me>
2023-09-05 17:23:35 -0300

    missingConditionsFix

--- Arduino/humiditySensor.ino
+++ Arduino/humiditySensor.ino
@@ -3,15 +3,39 @@
 const int humiditySensorPin = A0;
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
   pinMode(humiditySensorPin, INPUT);
 
 }
 
 void loop() {
+  if (analogRead(humiditySensorPin) > 750) {
+    Serial.println("[+] Umidade abaixo do nível ideal!\n");
+  };
 
-    /* Missing conditions here */
-  Serial.println(analogRead(humiditySensorPin));
-  delay(1000);
+  if (analogRead(humiditySensorPin) >= 450 && analogRead(humiditySensorPin) <= 750) {
+      Serial.println("[+] Umidade no nível ideal!\n");
+  };
+
+  if (analogRead(humiditySensorPin) < 450 ) {
+    Serial.println("[+] Umidade acima do nível!\n");
+  };
+
+    /* switch (analogRead(humiditySensorPin)) {
+    case analogRead(humiditySensorPin) > 750:
+      Serial.println("[+] Umidade abaixo do nível ideal!\n);
+      break;
+    case (analogRead(humiditySensorPin) >= 450 && analogRead(humiditySensorPin) <= 750:
+      Serial.println("[+] Umidade no nível ideal!\n");
+      break;
+    case analogRead(humiditySensorPin) < 450):
+      Serial.println("[+] Umidade acima do nível ideal!\n");
+      break;
+    default:
+      Serial.println("Sorry, I'm finding some issues with your input...");
+      break;
+  }; */
+
+  delay(1500);
 
 }
